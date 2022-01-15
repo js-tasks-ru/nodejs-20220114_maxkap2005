cf0b86246bccfbeff6b2bd6e06dd1bdce84f3565
maksymkaravaiev <maxkap2005@gmail.com>
2022-01-15 14:19:00 +0200

    First task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
 function sum(a, b) {
   /* ваш код */
+    if (typeof (a) === "number" && typeof (b) === "number"){
+        return a+b;
+    }
+    throw new TypeError("cda or b is not number");
 }
 
 module.exports = sum;
+
+
