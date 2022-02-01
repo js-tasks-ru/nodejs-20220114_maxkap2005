31464bb6d21be7b0489a519f39514c4399bb5c2a
maksymkaravaiev <maxkap2005@gmail.com>
2022-02-01 23:36:35 +0200

    Fifth task

--- 05-koajs/01-chat-app/app.js
+++ 05-koajs/01-chat-app/app.js
@@ -7,11 +7,46 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+let subscribers = [];
 
 router.get('/subscribe', async (ctx, next) => {
+    ctx.set('Content-Type', 'text/plain;charset=utf-8');
+    ctx.set("Cache-Control", "no-cache, must-revalidate");
+
+    const promise = new Promise((resolve, reject) => {
+        subscribers.push(resolve);
+        ctx.res.on("close", function () {
+            //console.log("rejected -> " + subscribers.indexOf(resolve));
+            subscribers.splice(subscribers.indexOf(resolve), 1);
+            //console.log(subscribers);
+            reject();
+        });
+    });
+
+
+    let message = await promise;
+    ctx.body = message;
+
+
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body.message;
+    if (message) {
+
+        subscribers.forEach(function (resolve) {
+            resolve(message);
+        });
+
+        subscribers = [];
+        ctx.response.status = 200;
+        ctx.body = "ok";
+    }
+    else {
+        ctx.response.status = 400;
+        ctx.body = "not ok";
+    }
+
 });
 
 app.use(router.routes());
